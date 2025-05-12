55bd51829fc90de61ff13c970d891fc0ed2c3a54
Ezequiel Messore <ezequielmessore@MacBook-Pro-de-Ezequiel.local>
2025-05-12 19:11:16 -0300

    feat: add MainActivity for setting up the app's main UI and navigation

---
+++ app/src/main/kotlin/br/com/messore/tech/lightning/nodes/presentation/MainActivity.kt
@@ -0,0 +1,35 @@
+package br.com.messore.tech.lightning.nodes.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import br.com.messore.tech.lightning.nodes.presentation.feature.nodes.NodesRoute
+import br.com.messore.tech.lightning.nodes.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        NodesRoute()
+                    }
+                }
+            }
+        }
+    }
+}
