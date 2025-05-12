911eb66805113f9fdb4e1885a4bf6d1cfaf0c6a1
Ezequiel Messore <ezequielmessore@MacBook-Pro-de-Ezequiel.local>
2025-05-12 19:37:01 -0300

    test: add TestCoroutineRule for managing coroutine dispatchers in unit tests

---
+++ app/src/test/java/br/com/messore/tech/lightning/nodes/presentation/view/model/TestCoroutineRule.kt
@@ -0,0 +1,33 @@
+package br.com.messore.tech.lightning.nodes.presentation.view.model
+
+import br.com.messore.tech.lightning.nodes.core.DispatchersProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class TestCoroutineRule : TestRule {
+
+    val testCoroutineDispatcher = StandardTestDispatcher()
+    val providers = DispatchersProvider(
+        io = testCoroutineDispatcher,
+        main = testCoroutineDispatcher,
+        default = testCoroutineDispatcher,
+        unconfined = testCoroutineDispatcher,
+    )
+
+    override fun apply(base: Statement, description: Description): Statement =
+        object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                Dispatchers.setMain(testCoroutineDispatcher)
+
+                base.evaluate()
+
+                Dispatchers.resetMain()
+            }
+        }
+}
