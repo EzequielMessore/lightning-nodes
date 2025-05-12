e5b6f033ac0b7c75173807df4ef508c948a79d81
Ezequiel Messore <ezequielmessore@MacBook-Pro-de-Ezequiel.local>
2025-05-12 19:23:56 -0300

    feat: add NodesViewState data class for managing node display states

---
+++ app/src/main/kotlin/br/com/messore/tech/lightning/nodes/presentation/feature/nodes/state/NodesViewState.kt
@@ -0,0 +1,21 @@
+package br.com.messore.tech.lightning.nodes.presentation.feature.nodes.state
+
+import br.com.messore.tech.lightning.nodes.domain.model.Node
+import br.com.messore.tech.lightning.nodes.presentation.feature.nodes.state.NodesViewIntent.OrderChange.Order
+
+data class NodesViewState(
+    val order: Order = Order.ALIAS,
+    val nodes: List<Node> = emptyList(),
+    val screenType: ScreenType = ScreenType.LOADING,
+    val bottomSheet: BottomSheet = BottomSheet(false),
+) {
+    fun success(nodes: List<Node>) = copy(nodes = nodes, screenType = ScreenType.SUCCESS)
+
+    fun error() = copy(screenType = ScreenType.ERROR)
+
+    fun loading() = copy(screenType = ScreenType.LOADING)
+}
+
+data class BottomSheet(
+    val isShowing: Boolean,
+)
