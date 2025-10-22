7a6c9697d354a5c43c7f9e87788336ab06339f40
Ezequiel Messore <ezequielmessore@MacBook-Pro-de-Ezequiel.local>
2025-10-22 15:25:12 -0300

    bump: update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "br.com.messore.tech.lightning.nodes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
